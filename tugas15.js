1ec949a9c9306837e8a0e5cd3755248fef980529
mazajiputra <mazajiputra@gmail.com>
2021-10-31 05:48:44 +0700

    Kumpul tugas

---
+++ tugas15.js
@@ -0,0 +1,13 @@
+var a=8;
+var b=6;
+
+function tugas(x,y) {
+    xk=Math.pow(x,2);
+    yk=Math.pow(y,2);
+    zk=xk+yk;
+    console.log("Nilai AB",x)
+    console.log("Nilai BC",y)
+    console.log("panjang sisi AC pada segitiga siku-siku tersebut adalah :",Math.sqrt(zk),"cm")
+}
+tugas(a,b)
+
